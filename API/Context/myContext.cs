5c40c7cab6a79d98dfac3520d4a72502d52a1751
Alfahmist <48145002+alfahmist@users.noreply.github.com>
2021-04-20 09:35:00 +0700

    Add project files.

---
+++ API/Context/myContext.cs
@@ -0,0 +1,19 @@
+﻿using API.Models;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+
+namespace API.Context
+{   
+    
+    public class myContext : DbContext
+    {
+        public myContext() : base("myConnection") { }
+        public DbSet<Division> Divisions { get; set; }
+        public DbSet<Department> Departments{ get; set; }
+
+    }
+}
